4470cc150f89f04b0a8bbf8de3c1da7158ac6e86
Thiago Henrique <thiago.silva1001@etec.sp.gov.br>
2020-06-17 19:44:26 -0300

    routes update and user control

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const routes = express.Router()
+
+const HomeController = require('../app/controllers/HomeController')
+
+const products = require('./products')
+const users = require('./users')
+
+routes.get('/', HomeController.index)
+
+routes.use('/products', products)
+routes.use('/users', users)
+
+// Alias (atalho)
+routes.get('/ads/create', function (req, res) {
+  return res.redirect('/products/create')
+})
+
+module.exports = routes
