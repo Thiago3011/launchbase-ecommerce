c2f93b420fc6cc1fd9bbf022e6c9d9b86534e56b
Thiago Henrique <thiago.silva1001@etec.sp.gov.br>
2020-06-16 22:08:55 -0300

    fix brazilian currency style

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -20,9 +20,13 @@ module.exports = {
     }
   },
   formatPrice (price) {
-    return new Intl.NumberFormat('pt-BR', {
+    const pricePreFormated = new Intl.NumberFormat('pt-BR', {
       style: 'currency',
       currency: 'BRL'
     }).format(price / 100)
+
+    const priceFormated = ((pricePreFormated).replace(',', '.')).slice(null, -3)
+
+    return priceFormated + ',00'
   }
 }
