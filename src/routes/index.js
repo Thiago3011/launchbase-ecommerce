baeb26fdc2f0c80e63842acfb4541a71367b53b5
Thiago Henrique <thiago.silva1001@etec.sp.gov.br>
2020-06-18 10:47:35 -0300

    user registration form create

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,4 +16,8 @@ routes.get('/ads/create', function (req, res) {
   return res.redirect('/products/create')
 })
 
+routes.get('/accounts', function (req, res) {
+  return res.redirect('/users/register')
+})
+
 module.exports = routes
--- src/routes/users.js
+++ src/routes/users.js
@@ -20,7 +20,7 @@ const UserController = require('../app/controllers/UserController')
 
 // // user register UserController
 
-// routes.get('/register', UserController.registerForm)
+routes.get('/register', UserController.registerForm)
 // routes.post('/register', UserController.post)
 
 // routes.get('/', UserController.show)
