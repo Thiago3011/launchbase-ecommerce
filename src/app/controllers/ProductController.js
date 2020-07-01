b1cac04c9443597539e9859f5f8ebd678334394a
Thiago Henrique <thiago.silva1001@etec.sp.gov.br>
2020-07-01 13:03:08 -0300

    validator fix

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -17,18 +17,6 @@ module.exports = {
   },
   async post (req, res) {
     try {
-      const keys = Object.keys(req.body)
-
-      for (const key of keys) {
-        if (req.body[key] === '') {
-          return res.send('Please, fill all fields!')
-        }
-      }
-
-      if (req.files.length === 0) {
-        return res.send('Please, send at least one image')
-      }
-
       let {
         category_id, name, description, old_price,
         price, quantity, status
@@ -87,14 +75,6 @@ module.exports = {
   },
   async put (req, res) {
     try {
-      const keys = Object.keys(req.body)
-
-      for (const key of keys) {
-        if (req.body[key] === '' && key !== 'removed_files') {
-          return res.send('Please, fill all fields!')
-        }
-      }
-
       if (req.files.length !== 0) {
         // validar se já não existem 6 imagens do banco de dados.
         const oldFiles = await Product.files(req.body.id)
