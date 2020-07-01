6a31dfad5c5d99e9971733c22b1f9f565342f84c
Thiago Henrique <thiago.silva1001@etec.sp.gov.br>
2020-07-01 12:27:45 -0300

    fix searchbox

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -12,31 +12,25 @@ module.exports = {
 
     return results.rows
   },
-  async search (params) {
-    const { filter, category } = params
-
-    let query = ''
-    let filterQuery = 'WHERE'
-
-    if (category) {
-      filterQuery = `${filterQuery}
-            products.category_id = ${category}
-            AND`
-    }
-
-    filterQuery = `
-            ${filterQuery}
-            products.name ilike '%${filter}%'
-            OR products.description ilike '%${filter}%'
-        `
-
-    query = `
+  async search ({ filter, category }) {
+    let query = `
             SELECT products.*,
                 categories.name AS category_name
             FROM products
             LEFT JOIN categories ON (categories.id = products.category_id)
-            ${filterQuery}
+            WHERE 1 = 1
         `
+
+    if (category) {
+      query += ` AND products.category_id = ${category}`
+    }
+
+    if (filter) {
+      query += ` AND (products.name ilike '%${filter}%' 
+      OR products.description ilike '%${filter}%')`
+    }
+
+    query += ' AND status != 0'
 
     const results = await db.query(query)
 
